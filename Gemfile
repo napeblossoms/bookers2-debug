850b4b979c3d67ef44f5349a9086d3e7adb84f20
EC2 Default User <ec2-user@ip-172-31-46-206.ec2.internal>
2022-04-25 08:47:54 +0000

    [Update-2]

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ group :development do
   # Can be configured to work on production as well see: https://github.com/MiniProfiler/rack-mini-profiler/blob/master/README.md
   gem 'rack-mini-profiler', '~> 2.0'
   gem 'listen', '~> 3.3'
+  gem 'image_processing', '~> 1.2'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -17,6 +18,7 @@ class BooksController < ApplicationController
       render 'index'
     end
   end
+
 
   def edit
     @book = Book.find(params[:id])
@@ -40,6 +42,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
