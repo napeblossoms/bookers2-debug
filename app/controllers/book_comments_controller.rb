c31437e28d2437ebf358ac9684f5e5e87003bd04
EC2 Default User <ec2-user@ip-172-31-46-206.ec2.internal>
2022-05-04 10:16:03 +0000

    [Add]非同期通信/コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,22 @@
 class BookCommentsController < ApplicationController
     def create
-      book = Book.find(params[:book_id])
+      @book = Book.find(params[:book_id])
       comment = current_user.book_comments.new(book_comment_params)
-      comment.book_id = book.id
-      if comment.save
-       redirect_to book_path(book)
-      else
-       redirect_to book_path(book)
-      end
+      comment.book_id = @book.id
+      comment.save
+      #if comment.save
+       #redirect_to request.referer
+      #else
+       #redirect_to book_path(book)
+      #end
     end
 
 
     def destroy
+    refroute = Rails.application.routes.recognize_path(request.referrer)
+    @book = Book.find(refroute[:id])
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    #redirect_to request.referer
     end
 
   private
